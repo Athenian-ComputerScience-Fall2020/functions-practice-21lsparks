dd772a336d18f2c7736a72080111271aed181d48
lspar <21lsparks@athenian.org>
2020-11-06 14:47:45 -0800

    DONE YEEEE BOISSS

--- add5.py
+++ add5.py
@@ -1,8 +1,9 @@
 def add_five(num1):
     print(num1 + 5)
 
-add_five(42)
-# user_input = int(input("Enter a number"))
-# add_five(user_input)
+#add_five(42)
+#add_five(2)
+user_input = int(input("Enter a number"))
+add_five(user_input)
 
-# add_five(int(input("Enter a number")))
+add_five(int(input("Enter a number")))
--- calculator.py
+++ calculator.py
@@ -1,4 +1,35 @@
 '''
 Collaborators: 
-
+# megan leich
 '''
+
+
+def add(num1, num2):
+    return num1 + num2
+
+
+def sub(num1, num2):
+    return num1 - num2
+
+
+def mult(num1, num2):
+    return num1 * num2
+
+
+def div(num1, num2):
+    return num1 / num2
+
+
+num1 = int(input("Enter a number or die: "))
+num2 = int(input("Enter a number or die: "))
+
+tomato = int(input("For adding, type 1, subtraction = type 2, multiplication = 3, division = 4:  "))
+
+if tomato == 1:
+    print(add(num1, num2))
+elif tomato == 2:
+    print(sub(num1, num2))
+elif tomato == 3:
+    print(mult(num1, num2))
+elif tomato == 4:
+    print(div(num1, num2))
